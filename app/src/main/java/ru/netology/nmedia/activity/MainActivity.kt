36067331f3c106fc55d890e6571f7566b9649f5f
aleksandrgrigorev <xundr1992@gmail.com>
2022-10-06 20:13:07 +0400

    RecyclerView.Adapter to ListAdapter

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         val adapter = PostsAdapter({viewModel.likeById(it.id)}, {viewModel.shareById(it.id)})
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.list = posts
+            adapter.submitList(posts)
         }
     }
 }
